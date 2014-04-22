d93e419a1e7f5c10e2e68fe3e82b42765dcf2459
Taiane <taiane.baldin@gmail.com>
2014-04-22 09:31:50 -0300

    Correção Bugs de inicialização de variáveis

--- main.c
+++ main.c
@@ -1,36 +1,27 @@
 // calculcadora Taiane e Diego
 #include <stdio.h>
+
 void porcent(float a, float b){
     float x;
     x = (b*100)/a;
-    printf("\n\nB e %2.f de A", x);
+    printf("\n\nB e %2.f%% de A", x);
 }
-
-
-
 
 int main (){
     float a, b;
-    printf("\nDigite um numero A:\n");
-    scanf("%d", &a);
-    printf("\nDigite um numero B:\n");
-    scanf("%d", &b);
-    int op,a,b;
+    int op;
 
-    printf("Digite a operacao: \n\t0 - Porcentagem\n\t1 -Teste se Primo\n\t2 - Mostra primos entre numeros\n\t3 - Média entre Numeros");
+    printf("digite um numero A:\n");
+    scanf("%f",&a);
+    printf("digite um numero B:\n");
+    scanf("%f",&b);
+
+    printf("Digite a operacao: \n\t0 - Porcentagem\n\t1 - Teste se Primo\n\t2 - Mostra primos entre numeros\n\t3 - Média entre Numeros\n");
     scanf("%d",&op);
-    porcent(a,b);
-
-    printf("digite um numero \n");
-    scanf("%d",a);
-    printf("digite segundo numero \n");
-    scanf("%d",b);
 
     switch (op){
         case 0:
-            //soma
-            printf("Porcentagem");
-
+            porcent(a,b);
             break;
         case 1:
             printf("Teste se Primo");
@@ -40,13 +31,10 @@ int main (){
         case 3:
             //mult
             printf("Mostra primos entre numeros");
-            multiplicacao(a,b);
-
             break;
 
         case 4:
             printf("Média entre Numeros");
-            divisao(a,b);
             break;
     }
 
