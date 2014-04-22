c642b195e5d528cca2d9c0374bbc4eb4fb4748a7
Taiane <taiane.baldin@gmail.com>
2014-04-22 09:22:26 -0300

    Inserção função porcentagem

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 // calculcadora Taiane e Diego
 #include <stdio.h>
-
-
-
+void porcent(float a, float b){
+    float x;
+    x = (b*100)/a;
+    printf("\n\nB e %2.f de A", x);
+}
 
 int main (){
-    printf("Ola Mundo");
+    float a, b;
+    printf("\nDigite um numero A:\n");
+    scanf("%d", &a);
+    printf("\nDigite um numero B:\n");
+    scanf("%d", &b);
 
+    porcent(a,b);
 
     return 0;
 
