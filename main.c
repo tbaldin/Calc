dabfe3936ccc80dc8717911f8918529e7ecf8c5a
Taiane <taiane.baldin@gmail.com>
2014-04-22 09:40:21 -0300

    Orreção de bugs

--- main.c
+++ main.c
@@ -9,8 +9,6 @@ void porcent(float a, float b){
 void mediaNumeros(int a,int b){
     float med;
     med = (a + b)/2;
-
-
 }
 
 
@@ -22,13 +20,6 @@ int main (){
     scanf("%d", &a);
     printf("\nDigite um numero B:\n");
     scanf("%d", &b);
-    int op;
-
-    printf("digite um numero A:\n");
-    scanf("%f",&a);
-    printf("digite um numero B:\n");
-    scanf("%f",&b);
-
     printf("Digite a operacao: \n\t0 - Porcentagem\n\t1 - Teste se Primo\n\t2 - Mostra primos entre numeros\n\t3 - Média entre Numeros\n");
     scanf("%d",&op);
 
@@ -38,22 +29,14 @@ int main (){
             break;
         case 1:
             printf("Teste se Primo");
-            //sub
             break;
-
         case 3:
-            //mult
             printf("Mostra primos entre numeros");
             break;
-
         case 4:
             printf("Média entre Numeros");
             mediaNumeros(a,b);
             break;
     }
-
     return 0;
-
-
-
 }
